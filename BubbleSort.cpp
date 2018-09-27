f43928f4df3e25dd0d5e523f1f7a90efdf183a78
mirco baalmans <mircobaalmans@live.nl>
2018-09-27 13:49:30 +0200

    all working

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,5 +1,16 @@
 #include "Bubblesort.h"
 
-std::vector<double> bubblesort(std::vector<double>){
-    
+std::vector<double> Bubblesort::bubblesort(std::vector<double> list){
+    int temp = 0;
+
+    for(int j = 0; j < list.size();j++){
+      for(int i = 1; i < list.size();i++){
+        if(list[i-1] > list[i]){
+          temp = list[i];
+          list[i] = list[i-1];
+          list[i-1] = temp;
+        }
+      }
+    }
+    return list;
 }
--- BubbleSort.h
+++ BubbleSort.h
@@ -1,6 +1,8 @@
 #ifndef BUBBLESORT_H
 #define BUBBLESORT_H
 
+#include <vector>
+
 class Bubblesort{
 public:
   static std::vector<double> bubblesort(std::vector<double> list);
